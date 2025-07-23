4034767d014637b12a878c3ad221b4b2d8059834
Lin Liu <linliu@student.hive.fi>
2025-07-23 14:40:03 +0300

    tester, error handle

--- src/signals/signals.c
+++ src/signals/signals.c
@@ -5,3 +5,12 @@ void	setup_signals(void)
 	signal(SIGINT, SIG_IGN); //If my process receives a SIGINT (Ctrl-C), just ignore it (SIG_IGN = ignore)
 	signal(SIGQUIT, SIG_IGN); //ignore SIGQUIT (usually triggered by Ctrl-\), so Ctrl-\ won’t kill my shell
 }
+
+/*void (*signal(int sig, void (*func)(int)))(int);
+It takes two parameters:
+
+    The signal number (sig)
+
+    The function to handle that signal (func)
+
+It returns the previous handler for that signal.*/

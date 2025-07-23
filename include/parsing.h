b263d6673c820caccb9608d3abeef8f8f83b734d
Lin Liu <linliu@student.hive.fi>
2025-07-23 17:16:46 +0300

    add libary

--- include/parsing.h
+++ include/parsing.h
@@ -4,8 +4,10 @@
 //readline            - get a line from a user with editing       get a line from a user with editing
 //#include <readline/history.h>       //add_history		//I dont need this, lin has it in main
 //#include <readline/readline.h>      //readline		//I dont need this, lin has it in main
+#include <stdlib.h>
+# include <stdlib.h>
 
-typedef enum e_token_type 
+typedef enum e_token_type
 {
 	T_WORD,             // 0    // "string" :   string, a cmd, a arguement
 	T_PIPE,             // 1    // |        :   pipe
@@ -33,13 +35,13 @@ typedef struct s_cmd
     char            *heredoc_delim; // For '<<' heredoc
     struct s_cmd    *next;          // Next command in a pipeline
 }                   t_cmd;
-//Parsing: - Taking the token list and Understanding the structure of the command, 
+//Parsing: - Taking the token list and Understanding the structure of the command,
 // 			- Grouping tokens into command nodes, pipes, redirections, etc,
 // 			- Building a linked list of command objects (t_cmd)
 
 //====================================for example====================================
 //echo "hello world" > file.txt | cat -e
-//[TOKEN: echo]	[TOKEN: "hello world"]	[TOKEN: >]	[TOKEN: file.txt]   [TOKEN: |] 	[TOKEN: cat]  	[TOKEN: -e] 
+//[TOKEN: echo]	[TOKEN: "hello world"]	[TOKEN: >]	[TOKEN: file.txt]   [TOKEN: |] 	[TOKEN: cat]  	[TOKEN: -e]
 //T_WORD	  				T_WORD		T_REDIRECT_OUT		 T_WORD			  T_PIPE	   T_WORD		  T_WORD
 //--------------------------------------------------------------------------------------
 //t_cmd *cmd_list = cmd1;
@@ -74,14 +76,14 @@ void    free_token_list(t_token *token);
 //this one moved to utils later
 void	*safe_malloc(size_t size);
 
-// parser.c       
-// syntax check (pipes, etc.)                  
+// parser.c
+// syntax check (pipes, etc.)
 int		check_syntax(t_token *tokens);
-// build command structures                    
+// build command structures
 t_cmd	*build_command_list(t_token *tokens);
-// handle redirections                        
+// handle redirections
 void	parse_redirections(t_cmd *cmd, t_token **tokens);
-// group commands by pipe                      
+// group commands by pipe
 t_cmd	*group_by_pipes(t_token *tokens);
 
 // parser_utils.c
--- include/utils.h
+++ include/utils.h
@@ -1,6 +1,8 @@
 #ifndef UTILS_H
 # define UTILS_H
 
+#include <stdlib.h>
+
 void	*safe_malloc(size_t size);
 
 #endif

bc67f19fc0df957711802fb62dad92bb0a22b756
Lin Liu <liulin0096@gmail.com>
2025-07-24 12:12:35 +0300

    combine tokenier inside repl, fix null check of malloc and strndup, tokenizer issue hasn't fixed

--- src/main.c
+++ src/main.c
@@ -2,27 +2,21 @@
 # include <stdio.h>
 
 //for testing, delete later
-static void print_token_list(t_token *head)
-{
-    while (head)
-    {
-        printf("[TYPE:%d] ", head->t_type);
-        if (head->quote_type == 1)
-            printf("(SINGLE-QUOTE) ");
-        else if (head->quote_type == 2)
-            printf("(DOUBLE-QUOTE) ");
-        printf("VAL:'%s'\n", head->str ? head->str : "(null)");
-        head = head->next;
-    }
-}
 
-int main(void)
-{
-    char    line[50] = "cat < file | grep foo > out.txt";
-    t_token *result;
+// int main(void)
+// {
+//     char    line[50] = "cat < file | grep foo > out.txt";
+//     t_token *result;
 
-    result = get_token_list(line);
-    printf("so far so good\n");
-    print_token_list(result);
-    free_token_list(result);
+//     result = get_token_list(line);
+//     printf("so far so good\n");
+//     print_token_list(result);
+//     free_token_list(result);
+// }
+
+int	main(void)
+{
+	setup_signals();
+	repl_loop();
+	return (0);
 }
--- src/repl/repl.c
+++ src/repl/repl.c
@@ -1,8 +1,21 @@
 #include "minishell.h"
 
+//for testing, delete later
+static void	print_token_list(t_token *token_list)
+{
+	int i = 0;
+	while (token_list)
+	{
+		printf("Token %d: %s (type=%d) (quoted=%d)\n", i, token_list->str, token_list->t_type, token_list->quote_type);
+		token_list = token_list->next;
+		i++;
+	}
+}
+
 void	repl_loop(void)
 {
 	char	*line;
+	t_token	*token_list;
 
 	while(1)
 	{
@@ -15,7 +28,10 @@ void	repl_loop(void)
 		if(*line)
 		{
 			add_history(line); //no need to care about the history memory
+			token_list = get_token_list(line);
 			printf("You enter: %s\n", line);
+			print_token_list(token_list);
+			free_token_list(token_list);
 			free(line);
 		}
 	}
